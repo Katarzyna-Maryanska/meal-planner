28931fecbfe279b1eaa5d4d7d7a34f66c300b338
Katarzyna Maryańska <47739183+Katarzyna-Maryanska@users.noreply.github.com>
2021-10-02 21:44:14 +0200

    setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import {
+  ThemeProvider,
+  ColorModeProvider,
+  CSSReset,
+  GlobalStyle,
+} from "@chakra-ui/react";
+
+import Layout from "../components/Layout";
+import { theme } from "../styles/theme";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+  <ThemeProvider theme={theme}>
+    <ColorModeProvider options={theme.config}>
+      <CSSReset />
+      <GlobalStyle />
+      <Layout title="Home | Next.js + TypeScript Example">
+        <h1>Hello Next.js 👋</h1>
+        <p>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </p>
+      </Layout>
+    </ColorModeProvider>
+  </ThemeProvider>
+);
 
-export default IndexPage
+export default IndexPage;
---
+++ styles/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    primary: "#217463",
+    secondary: "#d0e7d9",
+    highlights: "#53a789",
+  },
+});
